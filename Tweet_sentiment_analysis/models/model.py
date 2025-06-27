6fd191061e93163949df1478c3ebcdc44d532edf
Nishant Singh <166206623+NishantkSingh0@users.noreply.github.com>
2025-06-27 19:11:38 +0530

    Update model.py

--- Tweet_sentiment_analysis/models/model.py
+++ Tweet_sentiment_analysis/models/model.py
@@ -2,7 +2,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, LSTM, Dense
 
 def build_model():
-    model = Sequential([
+    model=Sequential([
         Embedding(input_dim=10000, output_dim=64, input_length=40),
         LSTM(64),
         Dense(1, activation='sigmoid')
