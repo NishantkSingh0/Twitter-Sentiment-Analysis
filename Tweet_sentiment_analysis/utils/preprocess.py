4bee37090849f2a5456d38b6b494a5be7627112b
Nishant Singh <166206623+NishantkSingh0@users.noreply.github.com>
2025-06-27 19:11:12 +0530

    Update preprocess.py

--- Tweet_sentiment_analysis/utils/preprocess.py
+++ Tweet_sentiment_analysis/utils/preprocess.py
@@ -1,26 +1,25 @@
 import re
 from tensorflow.keras.preprocessing.text import Tokenizer
 
-tokenizer = Tokenizer(num_words=10000, oov_token="<OOV>")
+tokenizer=Tokenizer(num_words=10000, oov_token="<OOV>")
 
 def clean_text(text):
-    text = text.lower()
-    text = re.sub(r"http\S+|@\S+|#\S+", "", text)
-    text = re.sub(r"[^a-z\s]", "", text)
+    text=text.lower()
+    text=re.sub(r"http\S+|@\S+|#\S+", "", text)
+    text=re.sub(r"[^a-z\s]", "", text)
     return text.strip()
 
 def preprocess_texts(df):
-    df = df.drop(columns=['id', 'date', 'flag', 'user'], errors='ignore')
-    df['clean_text'] = df['text'].apply(clean_text)
+    df=df.drop(columns=['id', 'date', 'flag', 'user'], errors='ignore')
+    df['clean_text']=df['text'].apply(clean_text)
 
     tokenizer.fit_on_texts(df['clean_text'])
-    sequences = tokenizer.texts_to_sequences(df['clean_text'])
+    sequences=tokenizer.texts_to_sequences(df['clean_text'])
     
-    # Filter out sequences longer than 40
-    filtered_sequences = []
-    filtered_labels = []
+    filtered_sequences=[]
+    filtered_labels=[]
     for seq, label in zip(sequences, df['target']):
-        if len(seq) <= 40:
+        if len(seq) < 40:        # Removed texts which have token length of 40 & above
             filtered_sequences.append(seq)
             filtered_labels.append(label)
 
